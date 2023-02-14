778e73fefdbd2b40075e1b7e3b9186f683604230
jean-pierre daverio <jeanpierre.daverio@orange.fr>
2023-02-14 21:02:09 +0100

    logic of user story redirectiojns

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,17 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  # before_action :configure_permitted_parameters, if: :devise_controller?
 
   def current_company
     return nil unless user_signed_in?
-    current_user.company
+    @company = current_user.company
+  end
+
+  def after_sign_in_path_for(resource)
+    if current_user.company != nil
+      "/dashboard"
+    else
+      "/companies/new"
+    end
   end
 end
