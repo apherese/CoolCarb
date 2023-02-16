30ca27919dca19772cb2e2046ad86027fe3e5a47
jean-pierre daverio <jeanpierre.daverio@orange.fr>
2023-02-16 11:48:29 +0100

    fixing footprint per employee

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
-  # before_action :configure_permitted_parameters, if: :devise_controller?
 
   def current_company
     return nil unless user_signed_in?
