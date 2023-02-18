002c7fc2dd1c93dd05c60cd0cadb074c3f89c42f
jean-pierre daverio <jeanpierre.daverio@orange.fr>
2023-02-18 14:00:53 +0100

    three charts

--- app/javascript/controllers/chart_controller.js
+++ app/javascript/controllers/chart_controller.js
@@ -13,7 +13,103 @@ export default class extends Controller {
 
   render() {
     new Chart(
+      document.getElementById('benchmark'),
+      {
+        type: "bar",
+        data: {
+          labels: ["Simulation : Benchmark - Bilan - Bilan après plan d'action et les trois objectifs"],
+          datasets: [ {
+            label: "Benchmark",
+            type: "bar",
+            backgroundColor: "#1EDD88",
+            data: [this.data.get("myValue0")],
+            stack: 1
+          }]
+        },
+        options: {
+          scales: {
+            yAxes: [{
+              id: "stacked_testY",
+              type: 'linear',
+              position: "left",
+              stacked: true,
+              display: true
+            }],
+            xAxes: [{
+              position: "bottom",
+              stacked: true,
+              display: true
+            }]
+          }
+        }
+      }
+    );
+
+    new Chart(
       document.getElementById('footprint'),
+      {
+        type: "bar",
+        data: {
+          labels: ["Simulation : Benchmark - Bilan - Bilan après plan d'action et les trois objectifs"],
+          datasets: [ {
+            label: "Benchmark",
+            type: "bar",
+            backgroundColor: "#1EDD88",
+            data: [this.data.get("myValue0")],
+            stack: 1
+          }, {
+            label: "Scope 1",
+            backgroundColor: "#DDFFBC",
+            data: [this.data.get("myValue1")],
+            stack: 2,
+          }, {
+            label: "Scope 2",
+            backgroundColor: "#FEFFDE",
+            data: [this.data.get("myValue2")],
+            stack: 2
+          }, {
+            label: "Scope 3",
+            backgroundColor: "#52734D",
+            data: [this.data.get("myValue3")],
+            stack: 2
+          }, {
+            label: "Objectif intermédiaire 2030",
+            backgroundColor: "#FFD3B4",
+            data: [this.data.get("myValue4")],
+            stack: 4
+          }, {
+            label: "Objectif intermédiaire 2040",
+            backgroundColor: "#FFD3B4",
+            data: [this.data.get("myValue5")],
+            stack: 5
+          }, {
+            label: "Objectif 2050 approuvé SBTi",
+            backgroundColor: "#FFD3B4",
+            data: [this.data.get("myValue6")],
+            stack: 6
+          }]
+        },
+        options: {
+          scales: {
+            yAxes: [{
+              id: "stacked_testY",
+              type: 'linear',
+              position: "left",
+              stacked: true,
+              display: true
+            }],
+            xAxes: [{
+              position: "bottom",
+              stacked: true,
+              display: true
+            }]
+          }
+        }
+      }
+    );
+
+    new Chart(
+      document.getElementById('footprint_tasks'),
       {
         type: "bar",
         data: {
