1df51d271e354129890ca412e56dd1aba7d94f23
Mohamed Berrada Souni <mohamed.berrada-souni@edhec.com>
2023-02-18 15:54:22 +0100

    ajout de tous les charts

--- app/javascript/controllers/chart_controller.js
+++ app/javascript/controllers/chart_controller.js
@@ -7,174 +7,187 @@ export default class extends Controller {
   static DEFAULT_OPTIONS = { responsive: true, maintainAspectRatio: false };
 
   connect() {
-    // console.log("coucou from chart controller");
     this.render();
   }
 
   render() {
-    new Chart(
-      document.getElementById('benchmark'),
-      {
-        type: "bar",
-        data: {
-          labels: ["Simulation : Benchmark - Bilan - Bilan après plan d'action et les trois objectifs"],
-          datasets: [ {
-            label: "Benchmark",
-            type: "bar",
-            backgroundColor: "#1EDD88",
-            data: [this.data.get("myValue0")],
-            stack: 1
-          }]
-        },
-        options: {
-          scales: {
-            yAxes: [{
-              id: "stacked_testY",
-              type: 'linear',
-              position: "left",
-              stacked: true,
-              display: true
-            }],
-            xAxes: [{
-              position: "bottom",
-              stacked: true,
-              display: true
-            }]
-          }
-        }
-      }
-    );
+    const benchmarkCanva = document.querySelector('canvas#benchmark')
+    const footprintCanva = document.querySelector('canvas#footprint')
+    const tasksCanva = document.querySelector('canvas#tasks')
 
-    new Chart(
-      document.getElementById('footprint'),
-      {
-        type: "bar",
-        data: {
-          labels: ["Simulation : Benchmark - Bilan - Bilan après plan d'action et les trois objectifs"],
-          datasets: [ {
-            label: "Benchmark",
-            type: "bar",
-            backgroundColor: "#1EDD88",
-            data: [this.data.get("myValue0")],
-            stack: 1
-          }, {
-            label: "Scope 1",
-            backgroundColor: "#DDFFBC",
-            data: [this.data.get("myValue1")],
-            stack: 2,
-          }, {
-            label: "Scope 2",
-            backgroundColor: "#FEFFDE",
-            data: [this.data.get("myValue2")],
-            stack: 2
-          }, {
-            label: "Scope 3",
-            backgroundColor: "#52734D",
-            data: [this.data.get("myValue3")],
-            stack: 2
-          }, {
-            label: "Objectif intermédiaire 2030",
-            backgroundColor: "#FFD3B4",
-            data: [this.data.get("myValue4")],
-            stack: 4
-          }, {
-            label: "Objectif intermédiaire 2040",
-            backgroundColor: "#FFD3B4",
-            data: [this.data.get("myValue5")],
-            stack: 5
-          }, {
-            label: "Objectif 2050 approuvé SBTi",
-            backgroundColor: "#FFD3B4",
-            data: [this.data.get("myValue6")],
-            stack: 6
-          }]
-        },
-        options: {
-          scales: {
-            yAxes: [{
-              id: "stacked_testY",
-              type: 'linear',
-              position: "left",
-              stacked: true,
-              display: true
-            }],
-            xAxes: [{
-              position: "bottom",
-              stacked: true,
-              display: true
+    if (benchmarkCanva) {
+      new Chart(
+        benchmarkCanva,
+        {
+          type: "bar",
+          data: {
+            labels: ["Simulation : Benchmark - Bilan - Bilan après plan d'action et les trois objectifs"],
+            datasets: [ {
+              label: "Benchmark",
+              type: "bar",
+              backgroundColor: "#1EDD88",
+              data: [this.data.get("myValue0")],
+              stack: 1
             }]
+          },
+          options: {
+            scales: {
+              yAxes: [{
+                id: "stacked_testY",
+                type: 'linear',
+                position: "left",
+                stacked: true,
+                display: true
+              }],
+              xAxes: [{
+                position: "bottom",
+                stacked: true,
+                display: true
+              }]
+            }
           }
         }
-      }
-    );
+      );
+    }
 
-    new Chart(
-      document.getElementById('footprint_tasks'),
-      {
-        type: "bar",
-        data: {
-          labels: ["Simulation : Benchmark - Bilan - Bilan après plan d'action et les trois objectifs"],
-          datasets: [ {
-            label: "Benchmark",
-            type: "bar",
-            backgroundColor: "#1EDD88",
-            data: [this.data.get("myValue0")],
-            stack: 1
-          }, {
-            label: "Scope 1",
-            backgroundColor: "#DDFFBC",
-            data: [this.data.get("myValue1")],
-            stack: 2,
-          }, {
-            label: "Scope 2",
-            backgroundColor: "#FEFFDE",
-            data: [this.data.get("myValue2")],
-            stack: 2
-          }, {
-            label: "Scope 3",
-            backgroundColor: "#52734D",
-            data: [this.data.get("myValue3")],
-            stack: 2
-          }, {
-            label: "Bilan APRES le plan d'action",
-            backgroundColor: "#98DDCA",
-            data: [this.data.get("myValue7")],
-            stack: 3
-          }, {
-            label: "Objectif intermédiaire 2030",
-            backgroundColor: "#FFD3B4",
-            data: [this.data.get("myValue4")],
-            stack: 4
-          }, {
-            label: "Objectif intermédiaire 2040",
-            backgroundColor: "#FFD3B4",
-            data: [this.data.get("myValue5")],
-            stack: 5
-          }, {
-            label: "Objectif 2050 approuvé SBTi",
-            backgroundColor: "#FFD3B4",
-            data: [this.data.get("myValue6")],
-            stack: 6
-          }]
-        },
-        options: {
-          scales: {
-            yAxes: [{
-              id: "stacked_testY",
-              type: 'linear',
-              position: "left",
-              stacked: true,
-              display: true
-            }],
-            xAxes: [{
-              position: "bottom",
-              stacked: true,
-              display: true
+    if (footprintCanva) {
+      new Chart(
+        footprintCanva,
+        {
+          type: "bar",
+          data: {
+            labels: ["Simulation : Benchmark - Bilan - Bilan après plan d'action et les trois objectifs"],
+            datasets: [ {
+              label: "Benchmark",
+              type: "bar",
+              backgroundColor: "#1EDD88",
+              data: [this.data.get("myValue0")],
+              stack: 1
+            }, {
+              label: "Scope 1",
+              backgroundColor: "#DDFFBC",
+              data: [this.data.get("myValue1")],
+              stack: 2,
+            }, {
+              label: "Scope 2",
+              backgroundColor: "#FEFFDE",
+              data: [this.data.get("myValue2")],
+              stack: 2
+            }, {
+              label: "Scope 3",
+              backgroundColor: "#52734D",
+              data: [this.data.get("myValue3")],
+              stack: 2
+            }, {
+              label: "Objectif intermédiaire 2030",
+              backgroundColor: "#FFD3B4",
+              data: [this.data.get("myValue4")],
+              stack: 4
+            }, {
+              label: "Objectif intermédiaire 2040",
+              backgroundColor: "#FFD3B4",
+              data: [this.data.get("myValue5")],
+              stack: 5
+            }, {
+              label: "Objectif 2050 approuvé SBTi",
+              backgroundColor: "#FFD3B4",
+              data: [this.data.get("myValue6")],
+              stack: 6
             }]
+          },
+          options: {
+            scales: {
+              yAxes: [{
+                id: "stacked_testY",
+                type: 'linear',
+                position: "left",
+                stacked: true,
+                display: true
+              }],
+              xAxes: [{
+                position: "bottom",
+                stacked: true,
+                display: true
+              }]
+            }
           }
         }
-      }
-    );
+      );
+    }
+
+    if (tasksCanva) {
+      new Chart(
+        tasksCanva,
+        {
+          type: "bar",
+          data: {
+            labels: ["Simulation : Benchmark - Bilan - Bilan après plan d'action et les trois objectifs"],
+            datasets: [ {
+              label: "Benchmark",
+              type: "bar",
+              backgroundColor: "#1EDD88",
+              data: [this.data.get("myValue0")],
+              stack: 1
+            }, {
+              label: "Scope 1",
+              backgroundColor: "#DDFFBC",
+              data: [this.data.get("myValue1")],
+              stack: 2,
+            }, {
+              label: "Scope 2",
+              backgroundColor: "#FEFFDE",
+              data: [this.data.get("myValue2")],
+              stack: 2
+            }, {
+              label: "Scope 3",
+              backgroundColor: "#52734D",
+              data: [this.data.get("myValue3")],
+              stack: 2
+            }, {
+              label: "Bilan APRES le plan d'action",
+              backgroundColor: "#98DDCA",
+              data: [this.data.get("myValue7")],
+              stack: 3
+            }, {
+              label: "Objectif intermédiaire 2030",
+              backgroundColor: "#FFD3B4",
+              data: [this.data.get("myValue4")],
+              stack: 4
+            }, {
+              label: "Objectif intermédiaire 2040",
+              backgroundColor: "#FFD3B4",
+              data: [this.data.get("myValue5")],
+              stack: 5
+            }, {
+              label: "Objectif 2050 approuvé SBTi",
+              backgroundColor: "#FFD3B4",
+              data: [this.data.get("myValue6")],
+              stack: 6
+            }]
+          },
+          options: {
+            scales: {
+              yAxes: [{
+                id: "stacked_testY",
+                type: 'linear',
+                position: "left",
+                stacked: true,
+                display: true
+              }],
+              xAxes: [{
+                position: "bottom",
+                stacked: true,
+                display: true
+              }]
+            }
+          }
+        }
+      );
+    }
+
+
+
+
 
     // new Chart(
     //   document.getElementById('footprint_target'),
