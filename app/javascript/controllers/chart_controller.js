752829687e38e8dcd4839bce1c4f512a5273be18
jean-pierre daverio <jeanpierre.daverio@orange.fr>
2023-02-23 23:45:32 +0100

    graphs done and underline in navbar taken away

--- app/javascript/controllers/chart_controller.js
+++ app/javascript/controllers/chart_controller.js
@@ -12,6 +12,7 @@ export default class extends Controller {
   }
 
   render() {
+    // console.log("coucou from controller chart")
     const benchmarkCanva = document.querySelector('canvas#benchmark')
     const footprintCanva = document.querySelector('canvas#footprint')
     const tasksCanva = document.querySelector('canvas#tasks')
@@ -161,174 +162,183 @@ export default class extends Controller {
       );
     }
 
+
     if (tasksCanva) {
       new Chart(
         tasksCanva,
         {
           type: "bar",
           data: {
-            labels: ["Benchmark - Bilan - Bilan après actions - Objectifs 2030-40-50"],
-            datasets: [ {
-              label: "Benchmark",
-              type: "bar",
-              backgroundColor: "#1EDD88",
-              data: [this.data.get("myValue0")],
-              stack: 1
+            labels: ["benchmark", "bilan", "bilan amélioré", "Obj. 2030", "Obj. 2040", "Obj. 2050"],
+            datasets: [{
+              backgroundColor:  "#CCF7BB",
+              data: [this.data.get("myValue0"), 0, 0, this.data.get("myValue4"), this.data.get("myValue5"), this.data.get("myValue6") ],
+              datalabels: {
+                align: 'end',
+                anchor: 'start'
+              }
             }, {
               label: "Scope 1",
-              backgroundColor: "#DDFFBC",
-              data: [this.data.get("myValue1")],
-              stack: 2,
+              backgroundColor: "#B2BAEB",
+              data: [0, this.data.get("myValue1"), 0, 0, 0, 0],
+              datalabels: {
+                align: 'center',
+                anchor: 'center'
+              }
             }, {
               label: "Scope 2",
-              backgroundColor: "#FEFFDE",
-              data: [this.data.get("myValue2")],
-              stack: 2
+              backgroundColor: "#778AFB",
+              data: [0, this.data.get("myValue2"), 0, 0, 0, 0],
+              datalabels: {
+                anchor: 'center',
+                align: 'center',
+              }
             }, {
               label: "Scope 3",
-              backgroundColor: "#52734D",
-              data: [this.data.get("myValue3")],
-              stack: 2
+              backgroundColor: "#40497E",
+              data: [0, this.data.get("myValue3"), 0, 0, 0, 0],
+              datalabels: {
+                anchor: 'center',
+                align: 'center',
+              }
             }, {
-              label: "Bilan APRES le plan d'action",
-              backgroundColor: "#98DDCA",
-              data: [this.data.get("myValue7")],
-              stack: 3
-            }, {
-              label: "Objectif intermédiaire 2030",
-              backgroundColor: "#FFD3B4",
-              data: [this.data.get("myValue4")],
-              stack: 4
-            }, {
-              label: "Objectif intermédiaire 2040",
-              backgroundColor: "#FFD3B4",
-              data: [this.data.get("myValue5")],
-              stack: 5
-            }, {
-              label: "Objectif 2050 approuvé SBTi",
-              backgroundColor: "#FFD3B4",
-              data: [this.data.get("myValue6")],
-              stack: 6
-            }]
+              label: "Scope 1+2+3",
+              backgroundColor: "#6070D8",
+              data: [0, 0, this.data.get("myValue7"), 0, 0, 0],
+              datalabels: {
+                anchor: 'end',
+                align: 'start',
+              }
+            }
+          ]
           },
           options: {
             plugins: {
-              title: {
-              display: true,
-              align: "start",
-              color: "#1D4728",
-              text: 'Tonnes de CO2/collaborateur',
-              padding: {
-                top: 10,
-                bottom: 40
-              },
+              datalabels: {
+                color: 'white',
+                display: true,
+                font: {
+                  weight: 'bold'
+                },
+                formatter: Math.round
+              }
             },
-              legend: {
-                  display: false,
-                  position: 'bottom',
-                  labels: {
-                    // This more specific font property overrides the global property
-                    font: {
-                        size: 18
-                    },
-                  },
+
+            // Core options
+            aspectRatio: 5 / 3,
+            layout: {
+              padding: {
+                top: 24,
+                right: 16,
+                bottom: 0,
+                left: 8
+              }
+            },
+            elements: {
+              line: {
+                fill: false
               },
-              scales: {
-                yAxes: [{
-                  id: "stacked_testY",
-                  type: 'linear',
-                  position: "left",
-                  stacked: true,
-                  display: true
-                }],
-                xAxes: [{
-                  position: "bottom",
-                  stacked: true,
-                  display: true
-                }]
+              point: {
+                hoverRadius: 7,
+                radius: 5
+              }
+            },
+            scales: {
+              x: {
+                stacked: true
+              },
+              y: {
+                stacked: true
               }
             }
           }
         }
       );
     }
-
-
-
-
-
-    // new Chart(
-    //   document.getElementById('footprint_target'),
-    //   {
-    //     type: "bar",
-    //     data: {
-    //       labels: ["2030", "2040", "2050"],
-    //       datasets: [ {
-    //         label: "Objectif intermédiaire 2030",
-    //         backgroundColor: "#FFD3B4",
-    //         data: [this.data.get("myValue4"), "0", "0"],
-    //         stack: 1
-    //       }, {
-    //         label: "Objectif intermédiaire 2040",
-    //         backgroundColor: "#FFD3B4",
-    //         data: ["0", this.data.get("myValue5"), "0"],
-    //         stack: 1
-    //       }, {
-    //         label: "Objectif 2050 approuvé SBTi",
-    //         backgroundColor: "#FFD3B4",
-    //         data: ["0", "0", this.data.get("myValue6")],
-    //         stack: 1
-    //       }]
-    //     },
-    //     options: {
-    //       scales: {
-    //         yAxes: [{
-    //           id: "stacked_testY",
-    //           type: 'linear',
-    //           position: "left",
-    //           stacked: true,
-    //           display: true
-    //         }],
-    //         xAxes: [{
-    //           position: "bottom",
-    //           stacked: true,
-    //           display: true
-    //         }]
-    //       }
-    //     }
-    //   }
-    // );
-
-    // new Chart(
-    //   document.getElementById('tasks'),
-    //   {
-    //     type: "bar",
-    //     data: {
-    //       labels: ["2013-2020"],
-    //       datasets: [ {
-    //         label: "Isoler les bâtiments",
-    //         backgroundColor: "#98DDCA",
-    //         data: [this.data.get("myValue7")],
-    //         stack: 1
-    //       }]
-    //     },
-    //     options: {
-    //       scales: {
-    //         yAxes: [{
-    //           id: "stacked_testY",
-    //           type: 'linear',
-    //           position: "left",
-    //           stacked: true,
-    //           display: true
-    //         }],
-    //         xAxes: [{
-    //           position: "bottom",
-    //           stacked: true,
-    //           display: true
-    //         }]
-    //       }
-    //     }
-    //   }
-    // );
-  };
+  }
 }
+
+
+
+//   labels: ["Emissions tCO2"],
+//   datasets: [ {
+//     label: "Benchmark",
+//     type: "bar",
+//     backgroundColor: "#1EDD88",
+//     data: [this.data.get("myValue0")],
+//     stack: 1
+//   }, {
+//     label: "Scope 1",
+//     backgroundColor: "#DDFFBC",
+//     data: [this.data.get("myValue1")],
+//     stack: 2,
+//   }, {
+//     label: "Scope 2",
+//     backgroundColor: "#FEFFDE",
+//     data: [this.data.get("myValue2")],
+//     stack: 2
+//   }, {
+//     label: "Scope 3",
+//     backgroundColor: "#52734D",
+//     data: [this.data.get("myValue3")],
+//     stack: 2
+//   }, {
+//     label: "Bilan APRES le plan d'action",
+//     backgroundColor: "#98DDCA",
+//     data: [this.data.get("myValue7")],
+//     stack: 3
+//   }, {
+//     label: "Objectif intermédiaire 2030",
+//     backgroundColor: "#FFD3B4",
+//     data: [this.data.get("myValue4")],
+//     stack: 4
+//   }, {
+//     label: "Objectif intermédiaire 2040",
+//     backgroundColor: "#FFD3B4",
+//     data: [this.data.get("myValue5")],
+//     stack: 5
+//   }, {
+//     label: "Objectif 2050 approuvé SBTi",
+//     backgroundColor: "#FFD3B4",
+//     data: [this.data.get("myValue6")],
+//     stack: 6
+//   }]
+// },
+// options: {
+//   plugins: {
+//     title: {
+//     display: true,
+//     align: "start",
+//     color: "#1D4728",
+//     text: 'Tonnes de CO2/collaborateur',
+//     padding: {
+//       top: 10,
+//       bottom: 40
+//     },
+//   },
+//     legend: {
+//         display: false,
+//         position: 'bottom',
+//         labels: {
+//           // This more specific font property overrides the global property
+//           font: {
+//               size: 18
+//           },
+//         },
+//     },
+//     scales: {
+//       yAxes: [{
+//         id: "stacked_testY",
+//         type: 'linear',
+//         position: "left",
+//         stacked: true,
+//         display: true
+//       }],
+//       xAxes: [{
+//         position: "bottom",
+//         stacked: true,
+//         display: true
+//       }]
+//     }
+//   }
+// }
