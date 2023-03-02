7aceab3d0aacbf13e02dde26554e77bbb7a567e7
jean-pierre daverio <jeanpierre.daverio@orange.fr>
2023-03-02 14:47:59 +0100

    dynamic form coded

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,4 +2,4 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import "bootstrap"
-
+import { FootprintCalculator } from './services/footprint_calculator'
--- app/services/footprint_calculation.rb
+++ app/services/footprint_calculation.rb
@@ -8,6 +8,7 @@ class FootprintCalculation
 
   def calculate
     @groups = 365 * @company.room_nb * @company.load_factor.fdiv(@company.length_of_stay)
+    taille_batiments_results = (@footprint.taille_batiments * EmissionFactors::BATIMENTS)
     gaz_result = (@footprint.gaz * EmissionFactors::GAZ)
     fioul_result = (@footprint.fioul * EmissionFactors::FIOUL)
     essence_result = (@footprint.essence * 1.4 * EmissionFactors::ESSENCE)
@@ -15,8 +16,7 @@ class FootprintCalculation
     electricite_result = (@footprint.electricite * EmissionFactors::ELECTRICITE)
     clients_fr_result = (@footprint.clients_fr.fdiv(100) * EmissionFactors::CLIENTFR * 0.0021 * @groups)
     clients_int_result = (@footprint.clients_int.fdiv(100) * EmissionFactors::CLIENTINT * @groups)
-    fournisseurs_result = (@footprint.fournisseurs * 0.01 * EmissionFactors::FOURNISSEURS)
-    taille_batiments_results = (@footprint.taille_batiments * EmissionFactors::BATIMENTS)
+    fournisseurs_result = (@footprint.fournisseurs * 1.2 * EmissionFactors::FOURNISSEURS)
     @footprint.scope_1 = (gaz_result + fioul_result + essence_result + gazole_result).round
     @footprint.scope_2 = electricite_result.round
     @footprint.scope_3 = (clients_fr_result + clients_int_result + fournisseurs_result + taille_batiments_results).round
