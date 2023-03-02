cd8c1f1a2f2856b4585f53a98c426d48bb916c27
jean-pierre daverio <jeanpierre.daverio@orange.fr>
2023-03-02 10:54:59 +0100

    defaut values of room nb =100, and load factor =70

--- db/migrate/20230217213447_add_load_factor_to_companies.rb
+++ db/migrate/20230217213447_add_load_factor_to_companies.rb
@@ -1,5 +1,5 @@
 class AddLoadFactorToCompanies < ActiveRecord::Migration[7.0]
   def change
-    add_column :companies, :load_factor, :float, default: 0.7, null: false
+    add_column :companies, :load_factor, :float, default: 70, null: false
   end
 end
